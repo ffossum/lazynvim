935bb5734c9111c7b0f0030e8704d460012b5875
Fredrik Fossum <fredrik.fossum@gmail.com>
2023-03-14 20:17:07 +0100

    norelativenumber for inactive windows, fix terminal mappings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.cmd([[autocmd WinEnter * exec &number==1 ? "set relativenumber" : ""]])
+vim.cmd([[autocmd WinLeave * set norelativenumber]])
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,15 @@
 return {
   "akinsho/toggleterm.nvim",
+  keys = {
+    { [[<c-\>]], [[<cmd>exe v:count1 . "ToggleTerm"<cr>]], desc = "Toggle terminal", mode = "n" },
+    { [[<c-\>]], [[<cmd>close<cr>]], desc = "Close terminal", mode = "t" },
+    { [[<c-`>]], [[<cmd>exe v:count1 . "ToggleTerm"<cr>]], desc = "Toggle terminal", mode = "n" },
+    { [[<c-`>]], [[<cmd>close<cr>]], desc = "Toggle terminal", mode = "t" },
+  },
+  config = true,
   opts = {
     direction = "vertical",
     size = 100,
     persist_size = false,
-    open_mapping = "<c-`>",
   },
 }
