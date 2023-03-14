787887894b52a0bb2496f46cd358eae9f1a461b7
Fredrik Fossum <fredrik.fossum@finn.no>
2023-03-14 17:45:03 +0100

    terminal width 100

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,29 +1,8 @@
 return {
   "akinsho/toggleterm.nvim",
-  -- config = function()
-  --   require("toggleterm").setup({})
-  -- end,
-  -- keys = {
-  --   {
-  --     "<C-`>",
-  --     function()
-  --       vim.cmd("ToggleTerm")
-  --     end,
-  --     desc = "Toggle terminal",
-  --   },
-  --   {
-  --     "<C-`>",
-  --     function()
-  --       vim.cmd("ToggleTerm")
-  --     end,
-  --     desc = "Toggle terminal",
-  --     mode = "t",
-  --   },
-  -- },
-
   opts = {
     direction = "vertical",
-    size = 80,
+    size = 100,
     persist_size = false,
     open_mapping = "<c-`>",
   },
