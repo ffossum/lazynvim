b13de482edf21a03fd2bcaba1ecd87bf056e3111
Fredrik Fossum <fredrik.fossum@finn.no>
2023-03-15 11:37:15 +0100

    fix metals autoload

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,33 +1,54 @@
+local function metals_status()
+  return vim.g["metals_status"] or ""
+end
+
 return {
-  "scalameta/nvim-metals",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-  keys = {
-    {
-      "<leader>mc",
-      function()
-        require("telescope").extensions.metals.commands()
-      end,
-      desc = "Metals Commands",
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_b = { "branch", metals_status },
+      },
     },
   },
-  config = function()
-    local metals_config = require("metals").bare_config()
+  {
+    "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    keys = {
+      {
+        "<leader>mc",
+        function()
+          require("telescope").extensions.metals.commands()
+        end,
+        desc = "Metals Commands",
+      },
+    },
+    init = function()
+      local metals_config = require("metals").bare_config()
 
-    -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-    metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- *READ THIS*
+      -- I *highly* recommend setting statusBarProvider to true, however if you do,
+      -- you *have* to have a setting to display this in your statusline or else
+      -- you'll not see any messages from metals. There is more info in the help
+      -- docs about this
+      metals_config.init_options.statusBarProvider = "on"
 
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", {
-      clear = true,
-    })
-    vim.api.nvim_create_autocmd("FileType", {
-      -- NOTE: You may or may not want java included here. You will need it if you
-      -- want basic Java support but it may also conflict if you are using
-      -- something like nvim-jdtls which also works on a java filetype autocmd.
-      pattern = { "scala", "sbt", "java" },
-      callback = function()
-        require("metals").initialize_or_attach(metals_config)
-      end,
-      group = nvim_metals_group,
-    })
-  end,
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", {
+        clear = true,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
 }
