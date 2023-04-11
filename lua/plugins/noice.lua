5c88f28a6163bcc115b801d2d87916eca0fad78d
Fredrik Fossum <fredrik.fossum@finn.no>
2023-04-11 12:29:19 +0200

    remove fugitive, re-enable noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,12 +2,6 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    cmdline = {
-      view = "cmdline",
-    },
-    messages = {
-      enabled = false,
-    },
     presets = {
       lsp_doc_border = true,
     },
