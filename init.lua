891e91b0a936d4929607d8dcdd63e2962c3182f6
Fredrik Fossum <fredrik.fossum@finn.no>
2023-09-19 12:27:25 +0200

    fix window-picker errors

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+  vim.opt.linespace = 2
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,47 @@
 return {
   {
     "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
     opts = {
-      fg_color = "#535c6a",
-      other_win_hl_color = "#a0c980",
+      highlights = {
+        statusline = {
+          focused = {
+            fg = "#535c6a",
+            bg = "#a0c980",
+            bold = true,
+          },
+          unfocused = {
+            fg = "#535c6a",
+            bg = "#a0c980",
+            bold = true,
+          },
+        },
+        winbar = {
+          focused = {
+            fg = "#535c6a",
+            bg = "#a0c980",
+            bold = true,
+          },
+          unfocused = {
+            fg = "#535c6a",
+            bg = "#a0c980",
+            bold = true,
+          },
+        },
+      },
     },
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    dependencies = { "s1n7ax/nvim-window-picker" },
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "s1n7ax/nvim-window-picker",
+    },
     opts = {
       filesystem = {
         group_empty_dirs = true,
